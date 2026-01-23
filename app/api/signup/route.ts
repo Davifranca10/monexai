d0ddc7306f85fc2e8e0fcc02b717c288d62e5fab
davi-franca <davifranca629@gmail.com>
2026-01-23 15:16:07 -0300

    teste-schema2

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -54,6 +54,7 @@ export async function POST(request: NextRequest) {
 
     const user = await prisma.user.create({
       data: {
+        id: crypto.randomUUID(),
         email,
         name: name || null,
         passwordHash,
